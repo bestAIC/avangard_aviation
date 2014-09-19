35fc8ea964e16e3dcfddff28e270f0a36799cc1e
bestAIC <ad@aic.ru>
2014-09-19 17:21:27 +0400

    ad@aic.ru
    Шапка - footer

---
+++ include/footer.php
@@ -0,0 +1,36 @@
+	<footer id="footer">
+		<div class="row">
+			<figure class="logo col-lg-4">
+				<img src="img/logo_footer.png" alt="logo">
+			</figure>
+			<div class="footer_nav col-lg-8">
+				<ul class="footer_menu clearfix">
+					<li><a href="#0">About Us</a></li>
+					<li><a href="#0">Engineering & Technical Services</a></li>
+					<li><a href="#0">PrivateFlights</a></li>
+					<li><a href="#0">Managed Aircraft</a></li>
+				</ul>
+				<div class="row footer_text-cont">
+					<div class="col-lg-4">
+						<span class="ico"><img src="img/ico_1.png" alt=""></span>
+						<p>Avangard Aviation GmbH Baden Aircraft Operations GmbH Montreal Avenue D423 D-77836 Rheinmünster, Germany</p>
+					</div>
+					<div class="col-lg-4">
+						<span class="ico"><img src="img/ico_2.png" alt=""></span>
+						<p><a href="tel:+49 7229 18699-0">Tel.: +49 7229 18699-0</a><br/> <a href="fax:+49 7229 18699-59">Fax.: +49 7229 18699-59</a></p>
+					</div>
+					<div class="col-lg-4">
+						<span class="ico"><img src="img/ico_3.png" alt=""></span>
+						<p><a href="mailto:info@avangard-aviation.com">info@avangard-aviation.com</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer_autor row">
+			<div class="col-lg-8 copyright">Avangard Avia 2014. © All rights reserved</div>
+			<div class="col-lg-4"><a href="http://aic.ru">Design by <img src="img/aic.png" alt=""></a></div>
+		</div>
+	</footer>
+</div>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,82 @@
+<?
+$pagename = 'job';
+$pagetitle = 'Вакансии';
+include('inc/head.php');
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="ru"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="ru"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="ru"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="ru"> <!--<![endif]-->
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title><?print $pagetitle ?> :: Авангард авиайшон</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!--<meta name="viewport" content="width=device-width,initial-scale=.75,maximum-scale=3.0,user-scalable=yes" />-->
+	<meta name="viewport" content="target-densitydpi=160dpi, maximum-scale=3.0, initial-scale=.75, user-scalable=yes">
+	<meta name="viewport" content="width=device-width">
+
+<!-- 1 -->
+	<script src="js/libs/modernizr.js"></script>
+
+<!-- 2 -->
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+
+<!-- 3 -->
+	<script src="http://yandex.st/jquery/1.11.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.11.1.min.js">\x3C/script>')</script>
+
+<!-- 4 -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/libs/bootstrap.min.js"></script>
+
+<!-- 5 -->
+	<script src="js/script.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+	<header id="header" class="color_1">
+		<div class="header_shadow"></div>
+		<div class="row header_top">
+			<nav class="col-lg-4 menu_top">
+				<a href="#0" class="menu_top-btn">
+					<span class="menu_top-ico"><span></span></span>
+					<span class="menu_top-txt">Menu</span>
+				</a>
+				<ul class="list_item">
+					<li><a href="#0"></a></li>
+					<li><a href="#0"></a></li>
+					<li><a href="#0"></a></li>
+					<li><a href="#0"></a></li>
+					<li><a href="#0"></a></li>
+				</ul>
+			</nav>
+			<div class="col-lg-4 logo"><a href="#0"><img src="img/logo.png"  alt="logo"></a></div>
+			<div class="col-lg-4 lang-b">
+				<div class="lang_wrap">
+					<a href="#0" class="lang_btn">Leave request</a>
+					
+					<div class="lang_dropdown dropdown">
+						<a class="lang_lnk current" data-toggle="dropdown">Eng</a>
+						<ul class="lang_dropdown-menu dropdown-menu">
+							<li class="b-lang__lnk">Rus</li>
+							<li class="b-lang__lnk">Chi</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row header_cont">
+			<div class="col-lg-offset-2 col-lg-8 header_cont-wrap">
+			</div>
+		</div>
+	</header>
