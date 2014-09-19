2251aa5b2c9d2d6a6111eae98e3c483d7199899a
bestAIC <ad@aic.ru>
2014-09-19 13:03:18 +0400

    ad@aic.ru
    Начальная сборка

--- index.php
+++ index.php
@@ -29,8 +29,6 @@
 	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.11.1.min.js">\x3C/script>')</script>
 
 <!-- 4 -->
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
 
@@ -39,15 +37,84 @@
 
 </head>
 <body>
-	<header>
-		<nav>
-			
-		</nav>
+<div class="container-fluid">
+	<header id="header">
+		<div class="header_top">
+			<div class="row">
+				<nav class="menu_top col-lg-4">
+					<div class="fl">
+						<span class="menu_top-btn">Menu</span>
+						<ul class="list_item">
+							<li><a href="#0"></a></li>
+							<li><a href="#0"></a></li>
+							<li><a href="#0"></a></li>
+							<li><a href="#0"></a></li>
+							<li><a href="#0"></a></li>
+						</ul>
+					</div>
+				</nav>
+				<div class="logo col-lg-4"><img src="img/logo.png" alt="logo"></div>
+				<div class="lang col-lg-4">
+					<div class="fr">
+						<span class="lang_btn fl">Leave request</span>
+						<ul class="lang_list fl">
+							<li><a href="#0">Eng</a></li>
+							<li><a href="#0">Rus</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="header_cont">
+			<div class="row">
+				<div class="header_cont-wrap col-md-offset-2 col-md-8">
+					<h1>Engineering  & Technical Services</h1>
+					<p>Avangard Aviation is a full-service aircraft engineering and technical center. Operating under Part 145 Maintenance Organization Approval Certificate DE.145.0473 issued by Luftfahrt-Bundeasamt, the aviation licensing and operational safety oversight organization of Germany, Avangard Aviation provides the finest in German engineering with a state-of-the-art facility and an ideal location in the center of Europe</p>
+				</div>
+			</div>
+		</div>
 	</header>
 	<section>
-		<article>
-			<h1>Hello world!</h1>
-		</article>
+		<div class="wrapper">
+			
+		</div>
 	</section>
+
+	<footer id="footer">
+		<div class="row">
+			<div class="logo col-lg-4">
+				<img src="img/logo.png" alt="logo">
+			</div>
+			<div class="footer_nav col-lg-8">
+				<ul class="footer_menu clearfix">
+					<li><a href="#0">About Us</a></li>
+					<li><a href="#0">Engineering & Technical Services</a></li>
+					<li><a href="#0">PrivateFlights</a></li>
+					<li><a href="#0">Managed Aircraft</a></li>
+				</ul>
+				<div class="row footer_text-cont">
+					<div class="col-lg-4">
+						<span class="icomoon"></span>
+						<p>Avangard Aviation GmbH Baden Aircraft Operations GmbH Montreal Avenue D423 D-77836 Rheinmünster, Germany</p>
+					</div>
+					<div class="col-lg-4">
+						<span class="icomoon"></span>
+						<p><a href="tel:+49 7229 18699-0">Tel.: +49 7229 18699-0</a><br/> <a href="fax:+49 7229 18699-59">Fax.: +49 7229 18699-59</a></p>
+					</div>
+					<div class="col-lg-4">
+						<span class="icomoon"></span>
+						<p><a href="mailto:info@avangard-aviation.com">info@avangard-aviation.com</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer_autor">
+			<div class="row">
+				<div class="col-lg-8">Avangard Avia 2014. © All rights reserved</div>
+				<div class="col-lg-4"><a href="http://aic.ru">Design by <img src="img/aic.png" alt=""></a></div>
+			</div>
+		</div>
+	</footer>
+</div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var touchdevice = false;
 
 $(function() {
 	if (!$('html').hasClass('no-touch')) touchdevice = true;
+
+	if ($(document).height() <= $(window).height()){
+		$("#footer").addClass("navbar-fixed-bottom");
+	}
 });
 
 (function($) {
