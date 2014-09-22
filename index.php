5c03909c8dc2cfd76dcaf746c5279a05d2820ed6
bestAIC <ad@aic.ru>
2014-09-22 16:07:07 +0400

    ad@aic.ru
    Правки

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $pagetitle = 'Avangard Aviation';
 include('include/header.php')
 ?>
 	<section class="collage first">
-		<div class="img-holder" id="animate_index_1"><img src="pic/pic_2.jpg" alt=""></div>
+		<div class="collage_img"><img src="pic/pic_2.jpg" alt=""></div>
 
 		<div class="about_text left">
 			<p>is the world leader in private aviation, with a portfolio of services</p>
@@ -12,14 +12,30 @@ include('include/header.php')
 		</div>
 	</section>
 	<section class="collage second">
-		<div class="row">
-			<div class="about_text">
+		<div class="about_text center">
+			<div class="about_text-wrap">
 				<h2>Private Flights</h2>
-				<p>is the world leader in private aviation, with a portfolio of services that includes fractional ownership, jet cards, aircraft management and charter.</p>
+				<p>is the world leader in private aviation, with a portfolio of services that includes fractional ownership, jet cards, aircraft management and charter.</p>
 				<div class="btn color_1">Learn more</div>
 			</div>
 		</div>
-		<div class="img-holder" id="animate_index_2"><img src="pic/pic_3.png" alt=""></div>
+		<div class="collage_img"><img id="animate_index_2" src="pic/pic_3.png" alt=""></div>
+	</section>
+	<section class="collage third">
+		<div class="about_text">
+			<h2>Engineering<br/>  & Technical Services</h2>
+			<p>is the world leader in private aviation, with a portfolio of services that includes fractional ownership, jet cards, aircraft management and charter.</p>
+			<div class="btn color_1">Learn more</div>
+		</div>
+		<div class="collage_img"><img id="animate_index_2" src="pic/pic_4.jpg" alt=""></div>
+	</section>
+	<section class="collage fourth">
+		<div class="about_text left">
+			<h2>Managed Aircraft</h2>
+			<p>is the world leader in private aviation, with a portfolio of services that includes fractional ownership, jet cards, aircraft management and charter.</p>
+			<div class="btn color_1">Learn more</div>
+		</div>
+		<div class="collage_img"><img id="animate_index_2" src="pic/pic_5.jpg" alt=""></div>
 	</section>
 
 <?
