a2ec3183a0ebc3a3e145bfce89f42647204cdd43
Phys <phys@aic.ru>
2014-09-22 12:54:06 +0400

    Паралакас правки
    Signed-off-by: Phys <phys@aic.ru>

--- js/script.js
+++ js/script.js
@@ -19,5 +19,25 @@ $(function() {
 	$.fn.animateIndexFirst = function(options){
 		var that = $(this);
 
+// Parallax
+
+	that.mousemove(function(e){
+	 	// Вычисляем ширину блока HTML
+		var w = $('html').width();
+		var h = $('html').height();    
+	 	// Вычисляем позицию курсора в пикселях относительно центра
+		var x = (w/2 - e.pageX)/50;
+		var y = (h/2 - e.pageY)/25;
+
+	 	// Используя делитель добиваемся разной скорости движения фона уровней
+		$('#bnr_1 .cloud').css({
+      left:-960+x,
+      top:-30+y
+    });
+
+	}); 
+
+
+
 	}
 })(jQuery);
