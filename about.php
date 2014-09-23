3207e86e62a4a0b5f9d0b31f1e78a20c3c88b8c1
bestAIC <ad@aic.ru>
2014-09-23 17:38:22 +0400

    ad@aic.ru
    Разделение функций анимации

--- about.php
+++ about.php
@@ -9,12 +9,12 @@ include('include/header.php')
 			<p>is the world leader in private aviation, with a portfolio of services</p>
 			<div class="btn color_1"><span>About us</span></div>
 		</div>
-		<div class="collage_img"><img id="animate_index_5" class="js-animate_to" src="pic/pic_8.png" alt=""><img id="animate_index_6" src="pic/pic_9.png" alt=""></div>
+		<div class="collage_img"><img id="animate_index_5" src="pic/pic_8.png" alt=""><img id="animate_index_6" src="pic/pic_9.png" alt=""></div>
 		<div class="collage_down js-collage_down"></div>
 	</section>
 	<section class="collage second">
 		<div class="about_text center">
-			<div class="about_text-wrap">
+			<div class="about_text-wrap js-animate_to">
 				<h2>Private Flights</h2>
 				<p>is the world leader in private aviation, with a portfolio of services that includes fractional ownership, jet cards, aircraft management and charter.</p>
 				<div class="btn color_1">Learn more</div>
