a67e91c9bb3f72b2ddc9f06d5c9fee54b3c61c0f
Phys <phys@aic.ru>
2014-09-23 16:32:37 +0400

    Правки
    Signed-off-by: Phys <phys@aic.ru>

--- js/script.js
+++ js/script.js
@@ -4,44 +4,62 @@ $(document).ready(function() {
 
 
 function animateCollage(){
-	
-	$('#js-cont-height').css({
-		position: 'relative',
-		height: windowH
-	});
-
 	var win = $(window),
 		windowH = win.height(),
+		contFirst = $('#js-cont-height'),
 		btn = $('.js-collage_down', contFirst),
+		imgTo = $('div.collage_img .js-animate_to'),
 		img1 = $('#animate_index_1'),
 		img2 = $('#animate_index_2'),
 		textCnt1 = $('#animate_index_3'),
 		textCnt2 = $('#animate_index_4'),
-		posA1 = img1.offset().top,
-		posA2 = textCnt1.offset().top;
+		posA1 = imgTo.offset().top,
+		posA2 = textCnt1.offset().top,
 		posA3 = textCnt2.offset().top;
 
-	win.on('scroll', function(){
-		var topScroll = win.scrollTop(),
-			scrollPos = topScroll + windowH;
-			posTopScroll = (topScroll + windowH/2);
+	contFirst.css({position: 'relative'});
+	contFirst.css({height: windowH});
 
-		if(scrollPos > posA1){
-			lock = 1;
-			img1.addClass('active').css({position: 'absolute', top: 0});
-			img2.addClass('active');
-		}
-		if(posTopScroll > posA2){
-			lock = 1;
-			textCnt1.css({opacity: 1});
-		}
-		if(posTopScroll > posA3){
-			lock = 1;
-			textCnt2.css({opacity: 1});
-		}
+	
+	win.on('scroll', myScroll1);
+	win.on('scroll', myScroll2);
+	win.on('scroll', myScroll3);
 
-		return false;
-	});
+	if(img1.lengt){
+		function myScroll1(){
+			var topScroll = win.scrollTop(),
+				scrollPos = topScroll + windowH;
+
+			if(scrollPos > posA1){
+				img1.addClass('active').css({position: 'absolute', top: 0});
+				img2.addClass('active');
+			}
+		}
+	}
+
+	if(textCnt1.length){
+		function myScroll2(){
+			var topScroll = win.scrollTop(),
+				scrollPos = topScroll + windowH/2;
+
+			if(scrollPos > posA2){
+				textCnt1.css({opacity: 1});
+			}
+			return false;
+		}
+	}
+
+	if(textCnt2.length){
+		function myScroll3(){
+			var topScroll = win.scrollTop(),
+				scrollPos = topScroll + windowH/2;
+
+			if(scrollPos > posA3){
+				textCnt2.css({opacity: 1});
+			}
+			return false;
+		}
+	}
 	
 	btn.on('click', function(){
 		$('body').animate({
